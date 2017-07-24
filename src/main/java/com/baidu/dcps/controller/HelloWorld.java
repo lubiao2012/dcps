6e3e96a8f0f90f722a2957e891c7434ecbdbba87
lubiao2012 <wangrun20088002@126.com>
2017-07-24 16:37:57 +0800

    add mvn annotation

--- src/main/java/com/baidu/dcps/controller/HelloWorld.java
+++ src/main/java/com/baidu/dcps/controller/HelloWorld.java
@@ -1,8 +1,12 @@
 package com.baidu.dcps.controller;
 
+import com.baidu.dcps.dao.entity.User;
 import com.google.gson.Gson;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
@@ -19,14 +23,31 @@ import java.util.Map;
 @Controller
 public class HelloWorld {
 
-    @RequestMapping("/helloworld")
+    @RequestMapping(value = "/helloworld/{id}", method = RequestMethod.GET)
     @ResponseBody
-    public String hello() {
+    public String hello(@PathVariable long id) {
         System.out.println("hello world");
+        System.out.println(id);
+
+        Gson gson = new Gson();
+        Map<String, String> result = new HashMap<String, String>();
+        result.put("name", "hello world");
+        result.put("id", String.valueOf(id));
+        return gson.toJson(result);
+    }
+
+    @RequestMapping(value = "/user/add", method = RequestMethod.POST)
+    @ResponseBody
+    public String getParam(@RequestBody User user) {
+
+        System.out.println(user.getName());
+        System.out.println(user.getAge());
+
         Gson gson = new Gson();
         Map<String, String> result = new HashMap<String, String>();
         result.put("name", "hello world");
         return gson.toJson(result);
+
     }
 
 }
---
+++ src/main/java/com/baidu/dcps/dao/entity/User.java
@@ -0,0 +1,30 @@
+package com.baidu.dcps.dao.entity;
+
+/**
+ * Description:
+ * User: wanngrun03@baidu.com
+ * Date: 2017-07-23
+ * Time: 下午3:51
+ */
+public class User {
+
+    private String name;
+
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
